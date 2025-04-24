cbaf29348efdd0b2ceff92d2a4c588e914c68534
artem <taranenko.artem.work@proton.me>
2025-04-24 17:01:21 +0300

    Text and a button. Moved index from public to root.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import "./app.css";
+
 function App() {
 
   return (
     <>
+      <div className="main-container">
+        <h1>Bomboclaat!</h1>
+        <button className="bomboclat-button">play</button>
+      </div>
     </>
   )
 }
